935febf104328bfda5bd4c38bcd117bda5606372
Rokas Kašinskas <yourokas@gmail.com>
2022-07-12 11:24:20 +0300

    cosmetic changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Home = () => {
     return (
         <div>
-            <ul>
+            <ul className='ul'>
                 <li>
                     <Link to="/Vehicles">Cars</Link>
                 </li>
