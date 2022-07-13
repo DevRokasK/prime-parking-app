fd582e8fa2b3575271f2b6c104ef73b3c7a9ed6c
Rokas Kašinskas <yourokas@gmail.com>
2022-07-13 15:51:06 +0300

    vehicle REST api calls in development

--- src/services/RestService.ts
+++ src/services/RestService.ts
@@ -1,5 +1,6 @@
 import { Permit, IPermitItem } from "../model/Permit";
 import { Vehicle, IVehicleItem } from "../model/Vehicle";
+import { ClassError, IErrorItem } from "../model/Error";
 import { IPrimeParkingService } from "./IPrimeParkingService";
 import { MockService } from "./MockService";
 
@@ -9,10 +10,11 @@ export class RestService extends MockService implements IPrimeParkingService {
     private key: string = "1tQXTjYCpANabg9VzwA5gGfYIfHihWrRQ5pRfyaOZZcJAzFuxgBQ7A==";
     private baseURL: string = "https://crudas20220613152222.azurewebsites.net";
 
+    // Vehicle REST api calls
     public async GetVehicles(): Promise<Vehicle[]> {
         let result: Vehicle[] = [];
         const request: Request = new Request(this.getRestApiUrl("cars"));
-        const response = await fetch(request);
+        const response = await fetch(request, { method: 'GET' });
         const vehiclesData: IVehicleItem[] = await response.json();
         if (vehiclesData) {
             vehiclesData.forEach(value => {
@@ -23,12 +25,58 @@ export class RestService extends MockService implements IPrimeParkingService {
         return result;
     }
 
+    public async PostVehicle(data: Vehicle) {
+        let postVehicle = this.getDataJSON(data);
+        const request: Request = new Request(this.getRestApiUrl("cars"));
+        const response = await fetch(request, { method: 'POST', body: postVehicle })
+        if (response.status === 200) {
+            return;
+        }
+        else {
+            const iError: IErrorItem = await response.json();
+            const error: Error = new ClassError(iError);
+            return error;
+        }
+    }
+
+    public async PutVehicle(data: Vehicle) {
+        let postVehicle = this.getDataJSON(data);
+        const request: Request = new Request(this.getRestApiUrl("cars?id=" + data.id));
+        const response = await fetch(request, { method: 'PUT', body: postVehicle })
+        if (response.status === 204) {
+            return;
+        }
+        else {
+            const iError: IErrorItem = await response.json();
+            const error: Error = new ClassError(iError);
+            return error;
+        }
+    }
+
+    public async DeleteVehicle(id: string) {
+        const request: Request = new Request(this.getRestApiUrl("cars?id=" + id));
+        const response = await fetch(request, { method: 'DETELE' })
+        const result = await response.json();
+        if (response.status === 204) {
+            return;
+        }
+
+        //throw new ClassError({error: result.error, message: result.message});
+
+        // else {
+        //     const iError: IErrorItem = await response.json();
+        //     const error: Error = new ClassError(iError);
+        //     return console.log(error);
+        // }
+    }
+
+    // Permit REST api calls
     public async GetPermits(): Promise<Permit[]> {
         let result: Permit[] = [];
         const request: Request = new Request(this.getRestApiUrl("permits"));
-        const response = await fetch(request);
+        const response = await fetch(request, { method: 'GET' });
         const permitsData: IPermitItem[] = await response.json();
-        if(permitsData) {
+        if (permitsData) {
             permitsData.forEach(value => {
                 const permit = new Permit(value);
                 result.push(permit);
@@ -37,7 +85,35 @@ export class RestService extends MockService implements IPrimeParkingService {
         return result;
     }
 
-    public getRestApiUrl(path:string): string {
+    public async PostPermit() {
+
+    }
+
+    public async PutPermit() {
+
+    }
+
+    public async DeletePermit() {
+
+    }
+
+    public getRestApiUrl(path: string): string {
         return `${this.baseURL}/api/${path}?code=${this.key}`;
+    }
+
+    public getDataJSON(data: Vehicle): string {
+        return JSON.stringify({
+            id: data.id,
+            carNumber: data.carNumber,
+            make: data.make,
+            model: data.model,
+            registrationYear: data.registrationYear,
+            registrationPlace: data.registrationPlace,
+            fuelType: data.fuelType,
+            enginePower: data.enginePower,
+            engineTorque: data.engineTorque,
+            color: data.color,
+            doors: data.doors
+        });
     }
 }
