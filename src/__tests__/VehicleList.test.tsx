d6893475cf3e2adf045991c61f4acebee4d35b24
Rokas Kašinskas <yourokas@gmail.com>
2022-08-28 12:41:00 +0300

    fix: console errors

--- src/__tests__/VehicleList.test.tsx
+++ src/__tests__/VehicleList.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { RootStore } from '../stores/RootStore';
 import { VehicleList } from '../components/vehicles/VehicleList';
+import { Vehicle } from '../model/Vehicle';
 
 describe("Vehicle list", () => {
     test('should render a vehicle list with no selected vehicles', async () => {
@@ -14,4 +15,35 @@ describe("Vehicle list", () => {
         const stringElement = screen.queryByText(/Selected: /i);
         expect(stringElement).not.toBeInTheDocument();
     });
+
+    test('should render a vehicle list with 2 selected vehicles', async () => {
+        const store = new RootStore();
+        const vehicle = new Vehicle({
+            carNumber: "abc", id: "sdfha", make: "bmw", model: "x5",
+            registrationYear: new Date(2, 2, 2022), registrationPlace: "Palanga", fuelType: "Diesel",
+            enginePower: 90, engineTorque: 200, color: "black", doors: 5
+        }, store.VehiclesStore);
+        store.VehiclesStore.SelectedVehicles.push(vehicle);
+        store.VehiclesStore.SelectedVehicles.push(vehicle);
+        const selectedText = "Selected: 2";
+
+        render(
+            <VehicleList store={store.VehiclesStore} gate={store.Gates} />
+        );
+
+        const stringElement = screen.queryByText(selectedText);
+        expect(stringElement).toBeInTheDocument();
+    });
+
+    test('should render a gate button', async () => {
+        const store = new RootStore();
+
+        render(
+            <VehicleList store={store.VehiclesStore} gate={store.Gates} />
+        );
+
+        const buttonElement = screen.getByText(/Gate/i);
+        expect(buttonElement).toBeInTheDocument();
+    });
+
 })
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
+import { initializeFileTypeIcons } from '@fluentui/react-file-type-icons';
+initializeFileTypeIcons();
+initializeIcons();
